f193329022c60951e06be1dfa494e93b1e520d39
Juwon Do <nobeldr13@gmail.com>
2022-11-29 13:07:26 -0600

    create three items

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+item1 = {color: "red", price: 100_000, weight: 50}
+item2 = {color: "blue", price: 70_000, weight: 30}
+item3 = {:color => "yellow", :price => 50_000, :weight => 10}
+
+p "Color is #{item1[:color]}, price is #{item1[:price]}, and weight is #{item1[:weight]}"
