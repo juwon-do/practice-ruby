eb9213ef52816172d0e37b15795cea2db8e04d64
Juwon Do <nobeldr13@gmail.com>
2022-11-29 14:24:33 -0600

    add attr_reader/writer, use input_option

--- store_item.rb
+++ store_item.rb
@@ -5,22 +5,29 @@ item3 = {:color => "yellow", :price => 50_000, :weight => 10}
 p "Color is #{item1[:color]}, price is #{item1[:price]}, and weight is #{item1[:weight]}"
 
 class Item
-  def initialize(input_color, input_price, input_weight)
-    @color = input_color
-    @price = input_price
-    @weight = input_weight
+  attr_reader :color, :price, :weight
+  attr_writer :color, :price, :weight
+
+  def initialize(input_option)
+    @color = input_option[:input_color]
+    @price = input_option[:input_price]
+    @weight = input_option[:input_weight]
   end
 
   def print_item_info
     p "Color is #{@color}, price is #{@price}, and weight is #{@weight}."
   end
-
-
+  
+  def inflated_price
+    @price*= 1.2
+  end 
 end 
 
-item1 = Item.new("red", 100_000, 70)
+item1 = Item.new(input_color: "red", input_price: 100_000, input_weight: 70)
 p item1.print_item_info
-item2 = Item.new("blue", 70_000, 50)
+item1.inflated_price
+p item1.print_item_info
+item2 = Item.new(input_color: "blue", input_price: 70_000, input_weight: 50)
 p item2.print_item_info
-item3 = Item.new("yellow", 50_000, 30)
+item3 = Item.new(input_color: "yellow", input_price: 50_000, input_weight: 30)
 p item3.print_item_info
