9ff3a214b7d07f884bce474441afff17f0d0e5b4
Juwon Do <nobeldr13@gmail.com>
2022-11-30 14:12:35 -0600

    make the file works

--- ex3.rb
+++ ex3.rb
@@ -1,8 +1,9 @@
-puts “Greetings! What is your year of origin?'
-origin == gets.chomp
+puts "Greetings! What is your year of origin?"
+origin = gets.chomp.to_i
 if origin < 1900
-  puts ‘That’s the past!’
-elseif origin > 1900 && origin < 2020
-  puts “That’s the present!”
+  puts "That’s the past!"
+elsif origin > 1900 && origin < 2020
+  puts "That’s the present!"
 elsif
-  puts “That’s the future!”
+  puts "That’s the future!"
+end
