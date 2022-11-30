5e8541e94f7677aa713d5646ccfa55665dded3d6
Juwon Do <nobeldr13@gmail.com>
2022-11-30 13:54:11 -0600

    debugging

--- exercise1.rb
+++ exercise1.rb
@@ -1,6 +1,7 @@
-put “Hello!
+puts "Hello!"
 	greeting = gets.chomp()
-if answer[“Arrr!“]
-	  puts “Go away, pirate.”
-	Elsif
-	  puts “Greetings, hater of pirates!”
+if greeting == "Arrr!"
+	  puts "Go away, pirate."
+else
+	  puts "Greetings, hater of pirates!"
+end
